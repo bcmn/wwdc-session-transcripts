19b5f411284b9dbd096a8108f27724e7859e0866
Ben Newcombe <bcmn96@gmail.com>
2018-06-12 20:18:39 +0100

    Updates parse_sessions.swift

--- tools/parse_sessions.swift
+++ tools/parse_sessions.swift
@@ -11,44 +11,43 @@ let filename: String
 let lineBreak = "\r\n"
 let mode: Mode
 
-switch Process.arguments.count {
+switch CommandLine.arguments.count {
 case 2:
-    filename = Process.arguments[1]
+    filename = CommandLine.arguments[1]
     mode = .info
-case 3 where Mode(rawValue: Process.arguments[2]) != nil:
-    filename = Process.arguments[1]
-    mode = Mode(rawValue: Process.arguments[2])!
+case 3 where Mode(rawValue: CommandLine.arguments[2]) != nil:
+    filename = CommandLine.arguments[1]
+    mode = Mode(rawValue: CommandLine.arguments[2])!
 default:
     print(
         "\nUsage: parse_sessions.swift path [mode]\n" +
-        "\n" +
-        "Valid modes:\n" +
-        "   - sessions\n" +
-        "   - info" as String
+            "\n" +
+            "Valid modes:\n" +
+            "   - sessions\n" +
+            "   - info" as String
     )
     exit(0)
 }
 
 // load and parse data
-@noreturn
-func exitWithError(error: String) {
+func exitWithError(error: String) -> Never {
     print(error)
     exit(1)
 }
 
-guard let data = NSData(contentsOfFile: filename)
-    else {
-    exitWithError("Error: Couldn't open '\(filename)'")
+guard let fileUrl = URL(string: filename),
+      let data = try? Data(contentsOf: fileUrl) else {
+        exitWithError(error: "Error: Couldn't open '\(filename)'")
 }
 
 typealias JSONDict = [String: AnyObject]
 
 guard let
-    jsonDict = try! NSJSONSerialization.JSONObjectWithData(data, options: []) as? JSONDict,
-    responseDict = jsonDict["response"] as? JSONDict,
-    sessionData = responseDict["sessions"] as? [JSONDict]
+    jsonDict = try! JSONSerialization.jsonObject(with: data, options: []) as? JSONDict,
+    let responseDict = jsonDict["response"] as? JSONDict,
+    let sessionData = responseDict["sessions"] as? [JSONDict]
     else {
-    exitWithError("Error: Couldn't parse JSON in '\(filename)'")
+        exitWithError(error: "Error: Couldn't parse JSON in '\(filename)'")
 }
 
 /// A single WWDC session
@@ -57,59 +56,57 @@ struct Session : CustomStringConvertible {
     var title: String
     var track: String
     var desc: String
-    
+
     var description: String {
         let lines = ["\(number):",
-                     "  :title: \(title)",
-                     "  :track: \(track)",
-                     "  :description: \(desc)"
+            "  :title: \(title)",
+            "  :track: \(track)",
+            "  :description: \(desc)"
         ]
-        return lines.joinWithSeparator(lineBreak)
+        return lines.joined(separator: lineBreak)
     }
 }
 
 
 
 // process JSON into list of sessions
-let sessions: [Session] = sessionData
+var sessions: [Session] = sessionData
     .filter {
         return ($0["type"] as? String) != "Lab"
     }
-    .flatMap {
+    .compactMap {
         guard let numberString = ($0["id"] ?? $0["number"]) as? String
             else { return nil }
-        
+
         guard let
             type = $0["type"]! as? String,
-            number = Int(numberString),
-            title = $0["title"] as? String,
-            track = $0["track"] as? String,
-            desc = $0["description"] as? String
+            let number = Int(numberString),
+            let title = $0["title"] as? String,
+            let track = $0["track"] as? String,
+            let desc = $0["description"] as? String
             else { return nil }
 
-        let filteredTitle = title
-            .stringByReplacingOccurrencesOfString(":", withString: "&#58;")
-            .stringByReplacingOccurrencesOfString("\"", withString: "&quot;")
-        
+        let filteredTitle = title.replacingOccurrences(of: ":", with: "&#58;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+
         let filteredDesc = desc
-            .stringByReplacingOccurrencesOfString(":", withString: "&#58;")
-            .stringByReplacingOccurrencesOfString("\"", withString: "&quot;")
-        
+            .replacingOccurrences(of: ":", with: "&#58;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+
         return Session(number: number, title: filteredTitle, track: track, desc: filteredDesc)
-    }
+}
 
 // derive tracks and types
 let tracks = Set(sessions.lazy.map { $0.track })
-let types = Set(sessionData.lazy.flatMap { $0["type"] as? String })
+let types = Set(sessionData.lazy.compactMap { $0["type"] as? String })
 
 // output
 switch mode {
 case .sessions:
-    let sortedSessions = sessions.sort { $0.number < $1.number }
-    print(sortedSessions.map({ "\($0)" }).joinWithSeparator(lineBreak))
+    let sortedSessions = sessions.sorted { $0.number < $1.number }
+    print(sortedSessions.map({ "\($0)" }).joined(separator: lineBreak))
 case .info:
     print("\(sessions.count) sessions")
-    print("Tracks: \(tracks.sort().joinWithSeparator(", "))")
-    print("Types: \(types.sort().joinWithSeparator(", "))")
+    print("Tracks: \(tracks.sorted().joined(separator: ", "))")
+    print("Types: \(types.sorted().joined(separator: ", "))")
 }
-
